a9407a068172819cf74775a2f3dabd259a967d29
Антон <antonolegovich2017@icloud.com>
2022-08-06 20:08:48 +0600

    Домашка строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        // Задание 1
+        String firstName = " Иван";
+        String middleMame = " Иванов";
+        String lastName = " Иванович";
+        String fullName = middleMame +firstName +lastName;
+        System.out.println("Ф.И.О." + fullName);
+
+
+        // Задание 2
+        String fullNameMax = fullName.toUpperCase();
+        System.out.println(" Данные ФИО сотрудника для заполнения отчета" + fullNameMax);
+
+        // Задание 3
+        fullName = " Иванов Семён Иванович";
+        fullName = fullName.replace( "ё","е");
+        System.out.println(fullName);
+
+
     }
 }
