1ac721f71271fcb5d4607ac3687756ff40cdde0a
Антон <antonolegovich2017@icloud.com>
2022-08-08 20:33:36 +0600

    Домашка строки

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,46 @@ public class Main {
 
 
         // Задание 1
-        String firstName = " Семён";
-        String middleMame = " Иванов";
-        String lastName = " Иванович";
-        String fullName = middleMame +firstName +lastName;
+        String firstName = "Иван ";
+        String middleMame = "Иванов ";
+        String lastName = "Иванович ";
+        String fullName = middleMame + firstName + lastName;
         System.out.println("Ф.И.О." + fullName);
 
 
         // Задание 2
         String fullNameMax = fullName.toUpperCase();
-        System.out.println(" Данные ФИО сотрудника для заполнения отчета" + fullNameMax);
+        System.out.println(" Данные ФИО сотрудника для заполнения отчета " + fullNameMax);
 
         // Задание 3
-        fullName = fullName.replace( "ё","е");
-        System.out.println(" Данные ФИО сотрудника" +fullName);
+        fullName = "Иванов Семён Семёнович";
+        String rename = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника" + fullName);
+
+        String[] cemen = fullName.split(" ");
+        for (String cema : cemen) {
+            if (cema.contains("ё")) {
+                String[] splitname = cema.split("");
+                for (int y = 0; y < splitname.length; y++) {
+                    if (splitname[y].equals("ё")) {
+                        splitname[y] = "е";
+                    }
+                    System.out.print(splitname[y]);
+                }
+                System.out.print(" ");
+            } else {
+                System.out.print(cema + " ");
+            }
+        }
 
 
     }
+
 }
+
+
+
+
+
+
+
